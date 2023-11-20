4d65d1cf97c280d3395197145d10edcdaa1fabfc
Cem Cebeci <cemcebeci99@gmail.com>
2023-11-20 15:32:32 +0100

    emit the fuzz_target as a static library at compile time and link with it at runtime.

--- fuzzer/fuzz_target.cpp
+++ fuzzer/fuzz_target.cpp
@@ -1,13 +1,12 @@
-#include <cstdint>
-#include <iostream>
+#include "stdio.h"
 
 // This is implemented by RLC.
 extern "C" void RLC_simulate();
 
 // This is called by RLC_simulate.
 // It's a stand-in for the sophisticated stuff we'd like to do in C.
-extern "C" void printint64_t_(const int64_t *message) {
-    std::cout << "Message: " << *message << "\n";
+extern "C" void printint64_t_(const __int64_t *message) {
+    printf("Message: %ld \n", *message);
 }
 
 int main() {
